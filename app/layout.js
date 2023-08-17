b36e991645bbc088040364f1fb04e38f1047fb25
malikgaurav626 <malikgaurav626@gmail.com>
2023-08-17 19:44:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -41,9 +41,9 @@ export default function RootLayout({ children }) {
           <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-T52373PH"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
+              height={"0"}
+              width={"0"}
+              style={{ display: "none", visibility: "hidden" }}
             ></iframe>
           </noscript>
           <MainBody>
